6f06b383f4be8d4c0208c909e6d91e7d0fa1c98a
whitneywind <78887901+whitneywind@users.noreply.github.com>
2023-06-06 13:13:15 -0700

    design updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,27 +8,27 @@ export default async function Home() {
   return (
     <div className='sm:w-3/4 max-w-4xl mx-16 md:mx-auto'>
       <Header />
-      <main className="flex sm:col-span-2 flex-col items-center justify-between">
-        <div>
-          <ul className='inline-flex text-sm space-x-8 my-3 tracking-wide text-gray-800'>
-            <li>Home</li>
-            <li>Art</li>
-            <li>Exploration</li>
-            <li>Culinary</li>
-          </ul>
-        </div>
-        <div className='bg-amber-300 w-full p-12 sm:flex border-y border-black'>
-          <h3 className='font-serif text-5xl text-center sm:text-left'>Stay curious.</h3>
-          <p className='hidden sm:block sm:w-2/3 mt-2 lg:ml-24 sm:text-justify lg:text-right lg:mt-6'><span className='underline'>Medium</span> is the place to discover `stories, thinking, and expertise from writers on any topic.</p>
-        </div>
-      </main>
-      <div className='max-w-full min-h-[35rem] grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 grid-rows-5 gap-4 my-4'>
+      <div className='w-full flex justify-center border-b border-black'>
+        <ul className='inline-flex text-sm space-x-8 my-3 tracking-wide text-gray-800'>
+          <li>Home</li>
+          <li>Art</li>
+          <li>Exploration</li>
+          <li>Culinary</li>
+        </ul>
+      </div>
+      <div className='max-w-full min-h-[35rem] grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 grid-rows-4 gap-4 my-4'>
         {posts.map((post, ind) => (
           <div key={ind} className={`art${ind} w-full shadow-lg border border-1 border-gray-200 p-5`}>
             <PostPreview key={ind} index={ind} post={post} />
           </div>
         ))}
       </div>
+      <main className="flex sm:col-span-2 flex-col items-center justify-between">
+        <div className='bg-amber-300 w-full p-12 sm:flex border-y border-black'>
+          <h3 className='font-serif text-5xl text-center sm:text-left'>Stay curious.</h3>
+          <p className='hidden sm:block sm:w-2/3 mt-2 lg:ml-24 sm:text-justify lg:text-right lg:mt-6'><span className='underline'>Medium</span> is the place to discover `stories, thinking, and expertise from writers on any topic.</p>
+        </div>
+      </main>
     </div>
   )
 }
